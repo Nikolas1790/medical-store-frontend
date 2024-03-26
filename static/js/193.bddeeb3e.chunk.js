0bec9809760f2b4460395ab65431881064f695f8
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-03-26 20:52:35 +0000

    Deploying to gh-pages from @ Nikolas1790/medical-store-frontend@ecf299abdaa24880663c7916fc800d4df682924c 🚀

--- static/js/193.bddeeb3e.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[193],{193:function(e,t,r){r.r(t),r.d(t,{default:function(){return a}});var n=r(184);function u(){return(0,n.jsx)("p",{children:"Dashboard"})}function a(){return(0,n.jsx)(u,{})}}}]);
-//# sourceMappingURL=193.bddeeb3e.chunk.js.map
--- static/js/842.5eacb72a.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[842],{842:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var r=n(184);function u(){return(0,r.jsx)("p",{children:"Login"})}function c(){return(0,r.jsx)(u,{})}}}]);
-//# sourceMappingURL=842.5eacb72a.chunk.js.map
