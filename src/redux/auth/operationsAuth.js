bc02abdb1b1d0cc03a169009ab839175a0ae29cc
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-04-12 21:33:50 +0300

    toast-login-error

--- src/redux/auth/operationsAuth.js
+++ src/redux/auth/operationsAuth.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import { toast } from 'react-toastify';
 
 axios.defaults.baseURL = 'https://medical-store-beckend.onrender.com/api';
 
@@ -22,6 +23,7 @@ export const logIn = createAsyncThunk(
       setAuthHeader(res.data.token);
       return res.data;
     } catch (error) {
+      toast.error('Please check the entered data again')
       return thunkAPI.rejectWithValue(error.message);
     }
   }
