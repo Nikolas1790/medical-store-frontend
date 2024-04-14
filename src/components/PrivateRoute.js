7020384087d8fe65569380706c7b4624523f98e2
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-04-14 15:09:11 +0300

    privat-routes-correct

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,10 +1,10 @@
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../hooks/useAyth';
 
-
-export const PrivateRoute = ({ component: Component, redirectTo = '/' }) => {
-  const { isLoggedIn, isRefreshing } = useAuth();
+export const PrivateRoute = ({ component: Component, redirectTo = '/login' }) => {
+  const { isLoggedIn, isRefreshing, token } = useAuth();
   const shouldRedirect = !isLoggedIn && !isRefreshing;
 
-  return shouldRedirect ? <Navigate to={redirectTo} /> : Component;
+  return !token ? <Navigate to='/login'/> :
+          (shouldRedirect ? <Navigate to={redirectTo} /> : Component);
 };
